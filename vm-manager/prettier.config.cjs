432c10847c5867a3806e08e7d186e18827e0f406
DerLev <levin.schroeren@gmail.com>
2025-01-13 15:53:37 +0100

    adding a nextjs app for vm management

---
+++ vm-manager/prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Options} */
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
+
+module.exports = config
